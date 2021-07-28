b366b7e1f130fe3a73cccae074ec74cd49901985
kstdbrv <kstdbrv1@gmail.com>
2021-07-28 22:15:43 +0300

    feat(font): add fonts

--- app/app.tsx
+++ app/app.tsx
@@ -1,7 +1,31 @@
 import "react-native-gesture-handler"
+
+import {StyleSheet, View, Text} from "react-native"
 import * as React from "react"
 import {NavigationContainer} from "@react-navigation/native"
 
-export default function App() {
-  return <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+export default function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text style={styles.textTitle}>This is a title text</Text>
+      </View>
+    </NavigationContainer>
+  )
 }
+/* eslint-disable react-native/no-color-literals */
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: "#373AF5",
+    flex: 1,
+    justifyContent: "center",
+  },
+  textTitle: {
+    alignItems: "center",
+    color: "#fff",
+    fontFamily: "Inter-Regular",
+    fontSize: 22,
+    justifyContent: "center",
+  },
+})
